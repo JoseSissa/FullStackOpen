7a00ff6718b0d46cf4c6cf2be7cae500f4225f53
Jose Sissa <sissamendoza@outlook.com>
2025-01-03 11:50:44 -0500

    Excersice 2.11

--- exercises/exercise-2.6-2.10/src/App.jsx
+++ exercises/exercise-2.6-2.10/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [showPersons, setShowPersons] = useState(persons)
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:3000/persons')
+      .then(response => response.json())
+      .then(data => {
+        setPersons(data)
+        setShowPersons(data)
+      })
+  }, [])
 
   const addName = e => {
     e.preventDefault()
@@ -22,6 +26,7 @@ const App = () => {
       return
     }
     setPersons([...persons, { name: newName, number: newNumber }])
+    setShowPersons([...persons, { name: newName, number: newNumber }])
     setNewName('')
     setNewNumber('')
   }
@@ -33,7 +38,6 @@ const App = () => {
       return person.name.toLowerCase().includes(value.toLowerCase())
     })
     setShowPersons(a)
-    
   }
 
   return (
