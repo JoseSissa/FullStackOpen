28cf238d7132398efbb0f6454fc740d77f7e924b
Jose Sissa <sissamendoza@outlook.com>
2025-01-27 15:16:46 -0500

    Unit test

--- react/src/components/Note.jsx
+++ react/src/components/Note.jsx
@@ -3,7 +3,7 @@ const Note = ({ note, toggleImportanceOf, deleteNote }) => {
     ? 'make not important' : 'make important'
 
   return (
-    <li>
+    <li className="note">
       {note.content} 
       <button onClick={toggleImportanceOf}>{label}</button>
       <button onClick={deleteNote}>Delete</button>
